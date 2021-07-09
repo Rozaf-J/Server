6413d79a0fe285fd17de4841c5d4ed67868def32
Oleg Kalyta <oleg@productcrafters.io>
2021-07-09 09:41:18 +0300

    added example fetch delete; cors on server; prettier

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const connect = require("./DB");
-const override = require("method-override");
-const express = require("express");
-const { body, validationResult } = require("express-validator");
-const app = express();
-const port = 3000;
+const connect = require('./DB')
+const override = require('method-override')
+const express = require('express')
+const cors = require('cors')
+const { body } = require('express-validator')
+const app = express()
+const port = 3000
 
-app.use(override("_method"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "pug");
+app.use(cors())
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.use(override('_method'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'pug')
 
-app.get("/add", connect.get_view_Users);
+app.get('/add', connect.get_view_Users)
 
 app.post(
-  "/add",
-  body("name").isString().isLength({ min: 2 }),
-  body("age").isNumeric(),
+  '/add',
+  body('name').isString().isLength({ min: 2 }),
+  body('age').isNumeric(),
   connect.add_view_Users
-);
+)
 
-app.delete("/add", connect.remove_view_user);
+app.delete('/add', (req, res) => {
+  console.log(req.body, req.params)
+  res.sendStatus(200)
+})
 
-app.put("/add", connect.update_view_user);
+app.put('/add', connect.update_view_user)
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
